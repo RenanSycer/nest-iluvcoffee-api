d09a7248abb19175e247af71b75488566765ffd0
Renan Fernandes <renansycer@outlook.com.br>
2021-07-18 21:58:08 -0300

    fix: removing useless imports

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,9 +1,4 @@
-import {
-  HttpException,
-  HttpStatus,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Repository } from 'typeorm';
